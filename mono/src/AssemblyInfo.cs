bdf791a9191875782590aaad61ff8a560f4fd884
Gleb Golubitsky <sectoid@gnolltech.org>
2012-07-14 16:03:53 +0300

    [MONO] Skeleton of C# miner
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

---
+++ mono/src/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+// -*- mode: csharp; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+
+[assembly: System.Reflection.AssemblyTitle("ICFPC2012")]
+[assembly: System.Reflection.AssemblyDescription("Simple Lambda Miner for .NET platform")]
+[assembly: System.Reflection.AssemblyCompany("Oscillating Steam Locomotives")]
+[assembly: System.Reflection.AssemblyCopyright("GPL 3.0")]
---
+++ mono/src/main.cs
@@ -0,0 +1,11 @@
+// -*- mode: csharp; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+
+namespace Icfpc {
+public static class Miner {
+
+  public static int Main(string[] argv) {
+    return 0;
+  }
+}
+
+}
