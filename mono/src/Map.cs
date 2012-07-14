eec4629c4dc746f3eefe0c6fd7233f45bcb2c016
Gleb Golubitsky <sectoid@gnolltech.org>
2012-07-14 22:52:16 +0300

    [MONO] VM evaluator draft
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- mono/src/Map.cs
+++ mono/src/Map.cs
@@ -29,6 +29,9 @@ public enum Command {
 
 [Serializable]
 public class Map {
+  private static Map first = null;
+  private static Map second = null;
+
   private Item[,] state = null;
 
   public static Map Read(TextReader src) {
@@ -63,7 +66,10 @@ public class Map {
         if(retVal.state[i,j] == Item.Robot) {
           retVal.X = j;
           retVal.Y = i;
-        };
+        }
+        else if(retVal.state[i,j] == Item.Lambda) {
+          retVal.LambdasLeft++;
+        }
       }
     }
 
@@ -88,18 +94,39 @@ public class Map {
       };
     }
     
+    Map.first = retVal;
+    Map.second = retVal.Clone();
 
-    System.GC.Collect(); // SPIKE!
+    System.GC.Collect(0); // SPIKE!
 
     return retVal;
   }
 
   public Map(int m, int n) {
     state = new Item[m, n];
-    M = m; N = n;
+    M = m; N = n; X = 0; Y = 0;
+    Score = 0; LambdasLeft = 0; LambdasCollected = 0;
     Water = 0; Flooding = 0; Waterproof = 10;
   }
 
+  public Map Clone() {
+    var other = new Map(M, N);
+    other.CopyFrom(this);
+    Array.Copy(state, 0, other.state, 0, state.Length);
+    return other;
+  }
+
+  private Map CopyFrom(Map other) {
+    X = other.X; Y = other.Y;
+    Score = other.Score; 
+    LambdasLeft = other.LambdasLeft; LambdasCollected = other.LambdasCollected;
+    Water = other.Water; Flooding = other.Flooding; Waterproof = other.Waterproof;
+    return this;
+  }
+
+  public int LambdasCollected { get; private set; }
+  public int LambdasLeft { get; private set; }
+  public int Score { get; private set; }
   public int M { get; private set; }
   public int N { get; private set; }
 
@@ -121,7 +148,7 @@ public class Map {
 
   public override string ToString() {
     var sb = new StringBuilder();
-    sb.AppendFormat("Map ({0} x {1}), robot position: ({2} x {3})\n", M, N, X, Y);
+    sb.AppendFormat("Map ({0} x {1}), robot position: ({2} x {3}), score = {4}\n", M, N, X, Y, Score);
     for(var i = M - 1; i >= 0; i--) {
       for(var j = 0; j < N; j++) {
         sb.Append((char)(state[i,j]));
@@ -130,6 +157,119 @@ public class Map {
     }
     sb.AppendFormat("Water: {0}, Flooding: {1}, Waterproof: {2}", Water, Flooding, Waterproof);
     return sb.ToString();
+  }
+  
+  private Map Rotate() {
+    return (Map.first == this) ? Map.second : Map.first;
+  }
+
+  public Map ExecuteCommand(Command cmd) {
+    var next = this.Rotate();
+
+    if(cmd == Command.Abort) {
+      Abort(); return this;
+    }
+
+    var nX = X;
+    var nY = Y;
+
+    switch(cmd) {
+      case Command.Left: nX--; break;
+      case Command.Right: nX++; break;
+      case Command.Up: nY++; break;
+      case Command.Down: nY--; break;
+    }
+
+    if(cmd != Command.Wait) {
+      this.Score--; // Movement attempt. Reducing score.
+    }
+
+    // Can't move out of map boundaries
+    if((nX < 0) || (nX >= this.N)) {
+      cmd = Command.Wait;
+    }
+    if((nY < 0) || (nY >= this.M)) {
+      cmd = Command.Wait;
+    }
+    
+    var nItem = this[nX,nY];
+    if((nItem == Item.Empty)
+       || (nItem == Item.Earth)
+       || (nItem == Item.Lambda)
+       || (nItem == Item.OpenLift)) {
+      
+      if(nItem == Item.Lambda) {
+        this.CollectLambda();
+      }
+
+      if(nItem == Item.OpenLift) {
+        this.EnterLift();
+      }
+      
+      this.Move(nX, nY);
+    }
+    else if(nItem == Item.Rock) {
+      if((cmd == Command.Right) && ((nX + 1) < this.N) && (this[nX + 1, nY] == Item.Empty)) {
+        this.Move(nX, nY);
+        this[nX + 1, nY] = Item.Rock;
+      }
+      else if((cmd == Command.Left) && ((nX - 1) >= 0) && (this[nX - 1, nY] == Item.Empty)) {
+        Move(nX, nY);
+        this[nX - 1, nY] = Item.Rock;
+      }
+    }
+
+    return next.Update(this);
+  }
+
+  private Map Update(Map old) {
+    CopyFrom(old);
+
+    // Map update loop
+    for(int i = 0; i < this.M; i++) {
+      for(int j = 0; i < this.N; j++) {
+        var x = j; var y = i;
+        
+        if((old[x, y] == Item.Rock) && (old[x,y - 1] == Item.Empty)) {
+          this[x,y] = Item.Empty;
+          this[x,y - 1] = Item.Rock;
+        } else if((old[x, y] == Item.Rock) && (old[x, y-1] == Item.Lambda) && (old[x+1,y] == Item.Empty) && (old[x+1,y-1] == Item.Empty)) {
+          this[x,y] = Item.Empty;
+          this[x+1, y-1] = Item.Rock;
+        } else if((old[x, y] == Item.Rock) && (old[x, y-1] == Item.Rock) && ((old[x+1,y] != Item.Empty) || (old[x+1,y-1] != Item.Empty)) && (old[x-1,y] == Item.Empty) && (old[x-1,y-1] == Item.Empty)) {
+          this[x,y] = Item.Empty;
+          this[x-1, y-1] = Item.Rock;
+        } else if((old[x, y] == Item.Rock) && (old[x, y-1] == Item.Lambda) && (old[x+1,y] == Item.Empty) && (old[x+1,y-1] == Item.Empty)) {
+          this[x,y] = Item.Empty;
+          this[x+1, y-1] = Item.Rock;
+        } else if((old[x,y] == Item.ClosedLift) && (old.LambdasLeft == 0)) {
+          this[x,y] = Item.OpenLift;
+        } else {
+          this[x,y] = old[x,y];
+        }
+      }
+    }
+
+    return this;
+  }
+
+  private void Move(int nX, int nY) {
+    this[X,Y] = Item.Empty;
+    this[nX,nY] = Item.Robot;
+    X = nX; Y = nY;
+  }
+
+  private void CollectLambda() {
+    LambdasCollected++; LambdasLeft--;
+    Score += 25;
+  }
+
+  private void EnterLift() {
+    Score += (50 * LambdasCollected);
+  }
+
+  private void Abort() {
+    Score += (25 * LambdasCollected);
   }
 }
 
